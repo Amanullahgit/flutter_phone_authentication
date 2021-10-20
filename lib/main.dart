5d8c30deb37af1de28a2282f25d4ba61f1c4a895
punit <daspunit3@gmail.com>
2021-10-21 01:33:42 +0530

    flutter_form_builder field components added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'login.dart';
+import './onboard/onboard_form.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +17,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return LoginScreen();
+    return CompleteForm();
   }
 }
