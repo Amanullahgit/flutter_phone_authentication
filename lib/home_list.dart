a53b63b5d6b074168a8a95ee2a017fba1fcbea2f
masihtamsoy <masihtamsoy@gmail.com>
2021-10-27 11:27:30 +0530

    Card added to home page to show JOb description

--- lib/home_list.dart
+++ lib/home_list.dart
@@ -16,22 +16,50 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: MaterialButton(
-          color: Theme.of(context).colorScheme.secondary,
-          onPressed: () {
-            Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => new QuestionJourney(screenIndex: 0)),
-                (route) => false);
-          },
-          child: const Text(
-            'Start Test',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-      ),
+      body: Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+          width: 300,
+          height: 250,
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              children: [
+                ListTile(
+                  // leading: Icon(Icons.arrow_drop_down_circle),
+                  title: const Text(
+                      'Delivery Boy/Delivery Executive/Delivery Driver/Freshers/Biker/Rider'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    '₹50,000 a month',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children: [
+                    MaterialButton(
+                      color: Theme.of(context).colorScheme.secondary,
+                      onPressed: () {
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    new QuestionJourney(screenIndex: 0)),
+                            (route) => false);
+                      },
+                      child: const Text(
+                        'Start Test',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+                // Image.asset('assets/card-sample-image-2.jpg'),
+              ],
+            ),
+          )),
     );
   }
 
