2d0d78e3c51bb1116acf473f1d19c30ced14d777
masihtamsoy <masihtamsoy@gmail.com>
2021-10-31 12:51:54 +0530

    animated_splash_screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_auth_project/home_list.dart';
 import 'package:provider/provider.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 import './common/theme.dart';
 import './models/eligibility.dart';
 
-import 'login.dart';
+// import 'login.dart';
 // import './onboard/onboard_form.dart';
 // import 'home_list.dart';
 
@@ -28,8 +30,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Dashhire',
       theme: appTheme,
-      home: LoginScreen(),
+      // home: HomeScreen(),
+      home: AnimatedSplashScreen(
+          duration: 3000,
+          splash: Icons.home,
+          nextScreen: HomeScreen(),
+          splashTransition: SplashTransition.fadeTransition,
+          backgroundColor: Colors.deepPurpleAccent),
       debugShowCheckedModeBanner: false,
     );
   }
