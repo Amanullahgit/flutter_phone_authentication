f4baf2fcb3312cabe5d60d9b705a72b8ad2df954
masihtamsoy <masihtamsoy@gmail.com>
2021-11-02 00:11:00 +0530

    Congrates page UI done

--- lib/congrates.dart
+++ lib/congrates.dart
@@ -1,42 +1,76 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import './form_builder/ques_journey.dart';
-import './models/eligibility.dart';
+import 'package:phone_auth_project/home_list.dart';
 import 'package:provider/provider.dart';
+import './models/eligibility.dart';
+import './home_list.dart';
 
-class Congrates extends StatefulWidget {
-  @override
-  _CongratesState createState() => _CongratesState();
-}
+class Congrates extends StatelessWidget {
+//Remark Logic
+  String resultPhrase(int resultScore) {
+    String resultText;
+    if (resultScore >= 41) {
+      resultText = 'You are awesome!';
+      print(resultScore);
+    } else if (resultScore >= 31) {
+      resultText = 'Pretty likeable!';
+      print(resultScore);
+    } else if (resultScore >= 21) {
+      resultText = 'You need to work more!';
+    } else if (resultScore >= 1) {
+      resultText = 'You need to work hard!';
+    } else {
+      resultText = 'This is a poor score!';
+      print(resultScore);
+    }
+    return resultText;
+  }
 
-class _CongratesState extends State<Congrates> {
-  String uid;
   @override
   Widget build(BuildContext context) {
-    // INFO: calculate mark score
-    // context.read<ExamEvaluateModal>().markScored();
-    // does not work here
-
+    int _markScored =
+        Provider.of<ExamEvaluateModal>(context, listen: false).mark_scored;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Result'),
-      ),
-      body: Column(
-        children: [
-          Center(
-              child: Text('${context.watch<ExamEvaluateModal>().mark_scored}')),
-          Text(
-              'Congrates, You have completed the test. Our executive will reach you')
-        ],
-      ),
-    );
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    final mark = 30;
+        appBar: AppBar(title: Text('Result')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                // resultPhrase(context.watch<ExamEvaluateModal>().mark_scored),
+                resultPhrase(_markScored),
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ), //Text
+              Text(
+                'Score $_markScored',
+                style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Our executive will reach out to you",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              //Text
+              MaterialButton(
+                child: Text(
+                  'Restart Quiz!',
+                ), //Text
+                textColor: Colors.blue,
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
+                      (route) => false);
+                },
+              ), //FlatButton
+            ], //<Widget>[]
+          ), //Column
+        ) //C,
+        );
   }
 }
+
+
+// When to use watch, works only in ""
+// context.watch<ExamEvaluateModal>().mark_scored
