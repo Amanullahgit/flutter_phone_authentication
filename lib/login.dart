a946eedda50e9c3248eafafb1f2f7e85a7e7a4b8
masihtamsoy <masihtamsoy@gmail.com>
2021-11-07 13:40:53 +0530

    Show icons of prefessions on grid

--- lib/login.dart
+++ lib/login.dart
@@ -51,129 +51,302 @@ class _LoginScreenState extends State<LoginScreen> {
       appBar: AppBar(
         title: Text('Dashhire'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            child: Row(
-              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: GridView.count(
+          padding: EdgeInsets.all(10),
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 3,
+          // Generate 100 widgets that display their index in the List.
+          children: [
+            Column(
               children: [
-                Column(
-                  children: [
-                    SvgPicture.network(
-                      'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/telesales-fa.svg',
-                      semanticsLabel: 'Telemarketing',
-                      placeholderBuilder: (BuildContext context) => Container(
-                          padding: const EdgeInsets.all(30.0),
-                          child: const CircularProgressIndicator()),
-                    ),
-                    Text('Telemarketing')
-                  ],
+                SvgPicture.network(
+                  'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/telesales-fa.svg',
+                  semanticsLabel: 'Telemarketing',
+                  placeholderBuilder: (BuildContext context) => Container(
+                      padding: const EdgeInsets.all(30.0),
+                      child: const CircularProgressIndicator()),
                 ),
-                Column(
-                  children: [
-                    SvgPicture.network(
-                      'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/operations-executive-fa.svg',
-                      semanticsLabel: 'A shark?!',
-                      placeholderBuilder: (BuildContext context) => Container(
-                          padding: const EdgeInsets.all(30.0),
-                          child: const CircularProgressIndicator()),
-                    ),
-                    Text("Operations Executive")
-                  ],
-                ),
-                Column(
-                  children: [
-                    SvgPicture.network(
-                      'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/back-office-fa.svg',
-                      semanticsLabel: 'A shark?!',
-                      placeholderBuilder: (BuildContext context) => Container(
-                          padding: const EdgeInsets.all(30.0),
-                          child: const CircularProgressIndicator()),
-                    ),
-                    Text("Back Office Executive")
-                  ],
-                ),
-                Column(
-                  children: [
-                    SvgPicture.network(
-                      'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/sales-fa.svg',
-                      semanticsLabel: 'A shark?!',
-                      placeholderBuilder: (BuildContext context) => Container(
-                          padding: const EdgeInsets.all(30.0),
-                          child: const CircularProgressIndicator()),
-                    ),
-                    Text("Sales / Business Development")
-                  ],
-                ),
-                Column(
-                  children: [
-                    SvgPicture.network(
-                      'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/delivery-fa.svg',
-                      semanticsLabel: 'A shark?!',
-                      placeholderBuilder: (BuildContext context) => Container(
-                          padding: const EdgeInsets.all(30.0),
-                          child: const CircularProgressIndicator()),
-                    ),
-                    Text("Delivery boy")
-                  ],
-                ),
-                Column(
-                  children: [
-                    SvgPicture.network(
-                      'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/customer-care-fa.svg',
-                      semanticsLabel: 'A shark?!',
-                      placeholderBuilder: (BuildContext context) => Container(
-                          padding: const EdgeInsets.all(30.0),
-                          child: const CircularProgressIndicator()),
-                    ),
-                    Text("Customer Care")
-                  ],
-                ),
+                Text(
+                  'Telemarketing',
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+                )
               ],
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(children: [
-                Container(
-                  margin: EdgeInsets.only(top: 40, right: 10, left: 10),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Enter 10 digit phone number',
-                      prefix: Padding(
-                        padding: EdgeInsets.all(4),
-                        child: Text('+91'),
-                      ),
-                      border: OutlineInputBorder(),
-                    ),
-                    maxLength: 10,
-                    keyboardType: TextInputType.number,
-                    controller: _controller,
-                  ),
-                )
-              ]),
-              Container(
-                margin: EdgeInsets.all(10),
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    RoundedButtonWidget(
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => OTPScreen(_controller.text)));
-                      },
-                      buttonText: 'Continue',
-                    ),
-                    Text('By continuing you agree to our Terms and Condition')
-                  ],
+            Column(
+              children: [
+                SvgPicture.network(
+                  'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/operations-executive-fa.svg',
+                  semanticsLabel: 'A shark?!',
+                  placeholderBuilder: (BuildContext context) => Container(
+                      padding: const EdgeInsets.all(30.0),
+                      child: const CircularProgressIndicator()),
                 ),
-              ),
-            ],
-          ),
-        ],
-      ),
+                Text(
+                  "Operations Executive",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SvgPicture.network(
+                  'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/back-office-fa.svg',
+                  semanticsLabel: 'A shark?!',
+                  placeholderBuilder: (BuildContext context) => Container(
+                      padding: const EdgeInsets.all(30.0),
+                      child: const CircularProgressIndicator()),
+                ),
+                Text(
+                  "Back Office Executive",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SvgPicture.network(
+                  'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/sales-fa.svg',
+                  semanticsLabel: 'A shark?!',
+                  placeholderBuilder: (BuildContext context) => Container(
+                      padding: const EdgeInsets.all(30.0),
+                      child: const CircularProgressIndicator()),
+                ),
+                Text(
+                  "Business Development",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SvgPicture.network(
+                  'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/delivery-fa.svg',
+                  semanticsLabel: 'A shark?!',
+                  placeholderBuilder: (BuildContext context) => Container(
+                      padding: const EdgeInsets.all(30.0),
+                      child: const CircularProgressIndicator()),
+                ),
+                Text(
+                  "Delivery boy",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SvgPicture.network(
+                  'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/customer-care-fa.svg',
+                  semanticsLabel: 'A shark?!',
+                  placeholderBuilder: (BuildContext context) => Container(
+                      padding: const EdgeInsets.all(30.0),
+                      child: const CircularProgressIndicator()),
+                ),
+                Text(
+                  "Customer Care",
+                  style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),
+                )
+              ],
+            ),
+          ]),
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //   children: [
+      //     Container(
+      //       child: GridView.count(crossAxisCount: 2, children: [
+      //         Column(
+      //           children: [
+      //             SvgPicture.network(
+      //               'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/telesales-fa.svg',
+      //               semanticsLabel: 'Telemarketing',
+      //               placeholderBuilder: (BuildContext context) => Container(
+      //                   padding: const EdgeInsets.all(30.0),
+      //                   child: const CircularProgressIndicator()),
+      //             ),
+      //             Text('Telemarketing')
+      //           ],
+      //         ),
+      //         Column(
+      //           children: [
+      //             SvgPicture.network(
+      //               'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/operations-executive-fa.svg',
+      //               semanticsLabel: 'A shark?!',
+      //               placeholderBuilder: (BuildContext context) => Container(
+      //                   padding: const EdgeInsets.all(30.0),
+      //                   child: const CircularProgressIndicator()),
+      //             ),
+      //             Text("Operations Executive")
+      //           ],
+      //         ),
+      //         Column(
+      //           children: [
+      //             SvgPicture.network(
+      //               'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/back-office-fa.svg',
+      //               semanticsLabel: 'A shark?!',
+      //               placeholderBuilder: (BuildContext context) => Container(
+      //                   padding: const EdgeInsets.all(30.0),
+      //                   child: const CircularProgressIndicator()),
+      //             ),
+      //             Text("Back Office Executive")
+      //           ],
+      //         ),
+      //         Column(
+      //           children: [
+      //             SvgPicture.network(
+      //               'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/sales-fa.svg',
+      //               semanticsLabel: 'A shark?!',
+      //               placeholderBuilder: (BuildContext context) => Container(
+      //                   padding: const EdgeInsets.all(30.0),
+      //                   child: const CircularProgressIndicator()),
+      //             ),
+      //             Text("Sales / Business Development")
+      //           ],
+      //         ),
+      //         Column(
+      //           children: [
+      //             SvgPicture.network(
+      //               'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/delivery-fa.svg',
+      //               semanticsLabel: 'A shark?!',
+      //               placeholderBuilder: (BuildContext context) => Container(
+      //                   padding: const EdgeInsets.all(30.0),
+      //                   child: const CircularProgressIndicator()),
+      //             ),
+      //             Text("Delivery boy")
+      //           ],
+      //         ),
+      //         Column(
+      //           children: [
+      //             SvgPicture.network(
+      //               'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/customer-care-fa.svg',
+      //               semanticsLabel: 'A shark?!',
+      //               placeholderBuilder: (BuildContext context) => Container(
+      //                   padding: const EdgeInsets.all(30.0),
+      //                   child: const CircularProgressIndicator()),
+      //             ),
+      //             Text("Customer Care")
+      //           ],
+      //         ),
+      //       ]),
+      //     ),
+      //     // child: Row(
+      //     //   // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //     //   children: [
+      //     //     Column(
+      //     //       children: [
+      //     //         SvgPicture.network(
+      //     //           'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/telesales-fa.svg',
+      //     //           semanticsLabel: 'Telemarketing',
+      //     //           placeholderBuilder: (BuildContext context) => Container(
+      //     //               padding: const EdgeInsets.all(30.0),
+      //     //               child: const CircularProgressIndicator()),
+      //     //         ),
+      //     //         Text('Telemarketing')
+      //     //       ],
+      //     //     ),
+      //     //     Column(
+      //     //       children: [
+      //     //         SvgPicture.network(
+      //     //           'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/operations-executive-fa.svg',
+      //     //           semanticsLabel: 'A shark?!',
+      //     //           placeholderBuilder: (BuildContext context) => Container(
+      //     //               padding: const EdgeInsets.all(30.0),
+      //     //               child: const CircularProgressIndicator()),
+      //     //         ),
+      //     //         Text("Operations Executive")
+      //     //       ],
+      //     //     ),
+      //     //     Column(
+      //     //       children: [
+      //     //         SvgPicture.network(
+      //     //           'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/back-office-fa.svg',
+      //     //           semanticsLabel: 'A shark?!',
+      //     //           placeholderBuilder: (BuildContext context) => Container(
+      //     //               padding: const EdgeInsets.all(30.0),
+      //     //               child: const CircularProgressIndicator()),
+      //     //         ),
+      //     //         Text("Back Office Executive")
+      //     //       ],
+      //     //     ),
+      //     //     Column(
+      //     //       children: [
+      //     //         SvgPicture.network(
+      //     //           'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/sales-fa.svg',
+      //     //           semanticsLabel: 'A shark?!',
+      //     //           placeholderBuilder: (BuildContext context) => Container(
+      //     //               padding: const EdgeInsets.all(30.0),
+      //     //               child: const CircularProgressIndicator()),
+      //     //         ),
+      //     //         Text("Sales / Business Development")
+      //     //       ],
+      //     //     ),
+      //     //     Column(
+      //     //       children: [
+      //     //         SvgPicture.network(
+      //     //           'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/delivery-fa.svg',
+      //     //           semanticsLabel: 'A shark?!',
+      //     //           placeholderBuilder: (BuildContext context) => Container(
+      //     //               padding: const EdgeInsets.all(30.0),
+      //     //               child: const CircularProgressIndicator()),
+      //     //         ),
+      //     //         Text("Delivery boy")
+      //     //       ],
+      //     //     ),
+      //     //     Column(
+      //     //       children: [
+      //     //         SvgPicture.network(
+      //     //           'https://olxjobs-static.aasaanjobs.net/candidate_web/OlxIntegration/customer-care-fa.svg',
+      //     //           semanticsLabel: 'A shark?!',
+      //     //           placeholderBuilder: (BuildContext context) => Container(
+      //     //               padding: const EdgeInsets.all(30.0),
+      //     //               child: const CircularProgressIndicator()),
+      //     //         ),
+      //     //         Text("Customer Care")
+      //     //       ],
+      //     //     ),
+      //     //   ],
+      //     // ),
+
+      //     Column(
+      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //       children: [
+      //         Column(children: [
+      //           Container(
+      //             margin: EdgeInsets.only(top: 40, right: 10, left: 10),
+      //             child: TextField(
+      //               decoration: InputDecoration(
+      //                 hintText: 'Enter 10 digit phone number',
+      //                 prefix: Padding(
+      //                   padding: EdgeInsets.all(4),
+      //                   child: Text('+91'),
+      //                 ),
+      //                 border: OutlineInputBorder(),
+      //               ),
+      //               maxLength: 10,
+      //               keyboardType: TextInputType.number,
+      //               controller: _controller,
+      //             ),
+      //           )
+      //         ]),
+      //         Container(
+      //           margin: EdgeInsets.all(10),
+      //           width: double.infinity,
+      //           child: Column(
+      //             children: [
+      //               RoundedButtonWidget(
+      //                 onPressed: () {
+      //                   Navigator.of(context).push(MaterialPageRoute(
+      //                       builder: (context) => OTPScreen(_controller.text)));
+      //                 },
+      //                 buttonText: 'Continue',
+      //               ),
+      //               Text('By continuing you agree to our Terms and Condition')
+      //             ],
+      //           ),
+      //         ),
+      //       ],
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
